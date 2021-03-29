cc63c4746f39fc13b2f548b8048001fefa7d69c2
gabrielgagz <g.a.gomez@gmail.com>
2021-03-29 20:59:06 -0300

    SERVER: Update queries according to new SQL schema
    Signed-off-by: gabrielgagz <g.a.gomez@gmail.com>

--- server/models/users.js
+++ server/models/users.js
@@ -28,9 +28,9 @@ const getUserById = (request, response) => {
 
 const createUser = (request, response) => {
 
-    const { firstname, lastname, nickname, profilepic } = request.body;
+    const { firstname, lastname, nickname, profilepic, password, email } = request.body;
     
-    pool.query('INSERT INTO users (user_firstname, user_lastname, user_nickname, user_profilepic) VALUES ($1, $2, $3, $4)', [firstname, lastname, nickname, profilepic], (error, results) => {
+    pool.query('INSERT INTO users (user_firstname, user_lastname, user_nickname, user_profilepic, user_password, user_email) VALUES ($1, $2, $3, $4, $5, $6)', [firstname, lastname, nickname, profilepic, password, email], (error, results) => {
 
         handleResponse( error, response, results );
 
@@ -41,9 +41,9 @@ const updateUser = (request, response) => {
 
     const id = parseInt(request.params.id);
 
-    const { firstname, lastname, nickname, profilepic } = request.body;
+    const { firstname, lastname, nickname, profilepic, password, email } = request.body;
     
-    pool.query('UPDATE users SET user_firstname = $1, user_lastname = $2, user_nickname = $3, user_profilepic = $4 WHERE user_id = $5', [firstname, lastname, nickname, profilepic, id], (error, results) => {
+    pool.query('UPDATE users SET user_firstname = $1, user_lastname = $2, user_nickname = $3, user_profilepic = $4, user_password = $5, user_email = $6 WHERE user_id = $7', [firstname, lastname, nickname, profilepic, password, email, id], (error, results) => {
 
         handleResponse( error, response, results );
 
