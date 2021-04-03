6e59f945241b7f766e24962c4e096c940375d9fc
gabrielgagz <g.a.gomez@gmail.com>
2021-04-03 13:47:04 -0300

    CLIENT: separate movement list logic
    Signed-off-by: gabrielgagz <g.a.gomez@gmail.com>

--- client/src/components/dashboard/DashBoardLayout.js
+++ client/src/components/dashboard/DashBoardLayout.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { DashBoardMovementsList } from './DashBoardMovementsList';
 import '../../css/dashboard.css';
-
 
 export const DashBoardLayout = ( {data} ) => {
 
     return (
         <div className='container'>
             <div className="row row-cols-1 row-cols-md-2 g-4 mt-5">
-                <div className="col mb-5">
+                <div className="col">
                     <div className="card shadow rounded border-0 card-left animate__animated animate__fadeInLeft">
                         <div className="card-body">
                             <p className="card-title">
@@ -46,44 +46,63 @@ export const DashBoardLayout = ( {data} ) => {
                             <hr />
                                 <ul className="list-group list-group-flush">
         
-                                {
-                                    // Check if data exists
-                                    ( data ) &&
-                                
-                                    // Render movements in a list
-                                    data.map( ( { movement_id, movement_amount, movement_date, movement_description, movement_type } ) => (
-                                        
-                                        <li className="list-group-item" key={`${ movement_id }`}>
-                                        
-                                            <div className="container-fluid my-2">
-                                                <div className="row row-cols-2">
-                                                    <div className="col w-75">
-                                                        <i className={`fas fa-circle text-${ movement_type } me-3`}></i>
-                                                        { movement_description }
-                                                    </div>
-                                                    <div className={ `col text-${ movement_type } w-25`}>
-                                                        ${ movement_amount }
-                                                    </div>
-                                                    <div className="col mt-0 fst-italic w-75">
-                                                    <i className='movement-type'></i>
-                                                        { movement_type }
-                                                    </div>
-                                                    
-                                                    <div className="col mt-0 fst-italic w-25">
-                                                        { 
-                                                            // If date is available, only get YYYY-MM-DD
-                                                            (movement_date) &&
-                                                                movement_date.toString().substr(0,10)
-                                                        }
-                                                    </div>
-                                                </div>
-                                            </div>
-                                    
-                                        </li>
-    
-                                    ) )
-                                    
-                                }
+                                    {
+                                        // Iterate and draw movements
+                                        data.map( ( data ) => {
+
+                                            return <DashBoardMovementsList data={ data } />
+
+                                        } )
+                                    }
+
+                                </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card shadow rounded border-0 card-movements animate__animated animate__fadeInRight">
+                        <div className="card-body">
+                            <p className="card-title">
+                                <i className="fa fa-bar-chart p-2" aria-hidden="true"></i>
+                                Ingress
+                            </p>
+                            <hr />
+                                <ul className="list-group list-group-flush">
+        
+                                    {
+                                        // Iterate and draw movements
+                                        data.map( ( data ) => {
+
+                                            if ( data.movement_type === 'ING' ) {
+                                                return <DashBoardMovementsList data={ data } />
+                                            }
+                                        } )
+                                    }
+
+                                </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card shadow rounded border-0 card-movements animate__animated animate__fadeInRight">
+                        <div className="card-body">
+                            <p className="card-title">
+                                <i className="fa fa-bar-chart p-2" aria-hidden="true"></i>
+                                Egress
+                            </p>
+                            <hr />
+                                <ul className="list-group list-group-flush">
+        
+                                    {
+                                        // Iterate and draw movements
+                                        data.map( ( data ) => {
+
+                                            if ( data.movement_type === 'EGR' ) {
+                                                return <DashBoardMovementsList data={ data } />
+                                            }
+                                        } )
+                                    }
+
                                 </ul>
                         </div>
                     </div>
