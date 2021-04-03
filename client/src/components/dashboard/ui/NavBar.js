05b8210db601be0e638bc1fc204c0eb76df1b436
gabrielgagz <g.a.gomez@gmail.com>
2021-04-03 03:40:08 -0300

    CLIENT: cleanup dashboard NavBar (refactor needed)
    Signed-off-by: gabrielgagz <g.a.gomez@gmail.com>

--- client/src/components/dashboard/ui/NavBar.js
+++ client/src/components/dashboard/ui/NavBar.js
@@ -8,40 +8,9 @@ export const NavBar = ( { user } ) => {
     const { profilepic } = user;
     
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-brand shadow py-3 px-0 animate__animated animate__fadeInDown">
-            <div className="container-fluid">
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a
-                                className="nav-link active"
-                                aria-current="page"
-                                href="/"
-                            >
-                                Profile
-                            </a>
-                        </li>
-                        <li className="nav-item mb-3">
-                            <a className="nav-link" href="/">
-                                Logout
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <img className='img-responsive nv-logo' src={ logo } alt='logo'/>
-                <img className='img-fluid' src={ `${profilepic}&color=ffffff&background=11b8a1&bold=true` } alt='User avatar'/>
-            </div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-brand shadow py-3 px-0 d-flex justify-content-between animate__animated animate__fadeInDown">
+            <img className='img-fluid nv-logo' src={ logo } alt='Logo'/>
+            <img className='img-fluid' src={ `${profilepic}&color=ffffff&background=11b8a1&bold=true` } alt='User avatar'/>
         </nav>
     );
 }
